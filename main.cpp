bee4f9e74aa59e5e402ac69cde14c0c23797650e
NicholasOsterfelt <nicholas.osterfelt@ucdenver.edu>
2015-10-26 23:51:39 -0600

    Updated for Pa3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include "Point.h"
 #include "Cluster.h"
 #include <assert.h>
-
+#include <fstream>
+#include "KMeans.h"
 using namespace std;
 using namespace Clustering;
         //function Prototypes
@@ -31,29 +32,99 @@ void pTestCGreaterEqualOperator();
 void pTestOstreamOperator();
 
 int main() {
-    pTestConstructor1();
-    pTestConstructor2();
-    pTestCopyConstructor();
-    pTestAssignmentOperator();
-    pTestGetDims();
-    pTestSetValue();
-    pTestGetValue();
-    pTestDistanceTo();
-    pTestMultiplyOperator1();
-    pTestDivideOperator1();
-    pTestMultiplyEqualOperator();
-    pTestDivideEqualOperator();
-    pTestAdditionOperator();
-    pTestSubtractionOperator();
-    pTestAdditionEqualOperator();
-    pTestSubtractionEqualOperator();
-    pTestCEqualOperator();
-    pTestCNEqualOperator();
-    pTestCLessOperator();
-    pTestCGreaterOperator();
-    pTestCLessEqualOperator();
-    pTestCGreaterEqualOperator();
-    pTestOstreamOperator();
+//    pTestConstructor1();
+//    pTestConstructor2();
+//    pTestCopyConstructor();
+//    pTestAssignmentOperator();
+//    pTestGetDims();
+//    pTestSetValue();
+//    pTestGetValue();
+//    pTestDistanceTo();
+//    pTestMultiplyOperator1();
+//    pTestDivideOperator1();
+//    pTestMultiplyEqualOperator();
+//    pTestDivideEqualOperator();
+//    pTestAdditionOperator();
+//    pTestSubtractionOperator();
+//    pTestAdditionEqualOperator();
+//    pTestSubtractionEqualOperator();
+//    pTestCEqualOperator();
+//    pTestCNEqualOperator();
+//    pTestCLessOperator();
+//    pTestCGreaterOperator();
+//    pTestCLessEqualOperator();
+//    pTestCGreaterEqualOperator();
+//    pTestOstreamOperator();
+//     Cluster c1 = Cluster(3);
+//     Cluster c2 = Cluster(3);
+// //    Cluster *cone = &c1;
+// //    Cluster *ctwo = &c2;
+//     Point *p1 = new Point(5);
+//    Point *p2 = new Point(3);
+//    Point *p3 = new Point(3);
+//    Point *p4 = new Point(3);
+//    Point *p5 = new Point(3);
+//    Point *p6 = new Point(3);
+//    p1->setValue(1,1);
+//    p2->setValue(1,2);
+//    p3->setValue(1,3);
+//    p4->setValue(1,4);
+//    p5->setValue(1,5);
+//    p6->setValue(1,6);
+//    c2.add(p6);
+//    c2.add(p5);
+//    c2.add(p4);
+//    c1.add(p1);
+//    c1.add(p2);
+//    c1.add(p3);
+//    cout << "this is c1" << endl << c1 << endl;
+//    cout << "this is c2" << endl << c2 << endl;
+//    c2.add(c1.remove(p1));
+//    Cluster::Move m(p1,cone, ctwo);
+//    ifstream is;
+//    is.open("input.txt");
+//    string line;
+//    istream & id = is;
+//
+//    is >> c1;
+//    cout << "this is c1" << endl << c1 << endl;
+
+    KMeans k(2,3);
+    KMeans k1(6,4);
+    KMeans k2(3,5);
+//     double p = k.computeClusteringScore();
+//     cout << p;
+
+//    c1.computeCentroid();
+//
+//
+//    PointPtr *ptArray = new PointPtr[5];
+//    c1.pickPoints(5,ptArray);
+//    cout << "These are the centroid picked points " << endl;
+//    for(int n = 0; n < 5; n++)
+//    {
+//        cout << *(*(ptArray+n)) << endl;
+//    }
+//    cout << "this is c1 centroid: " << c1.getCentroid() << endl;
+//    Cluster c3(c1);
+
+//    c2.remove(p5);
+//
+//    cout << "this is c1" << endl << c1 << endl;
+//    cout << "this is c1 centroid " << c1.getCentroid() << endl;
+//    c1.setCentroid(*p1);
+//    cout << "this is c1 centroid now " << c1.getCentroid() << endl;
+////    c2.add(c1.remove(p1));
+//
+////    c2+=c1;
+//
+//    cout << c1 + c2 << endl;
+//
+//    cout << "this is c1" << endl << c1 << endl;
+//    cout << "this is c2" << endl << c2 << endl;
+//    c2-=c1;
+//    cout << "this is c2" << endl << c2 << endl;
+    return 0;
 }
 void pTestConstructor1() {
     Point p(2);
